1328bab8a9f6ce29c26e9385181a37b813a104ff
max2oi <max2oi4work@gmail.com>
2017-02-28 19:38:39 +0800

    取消了+号变空格的功能, 因为某些pdf书籍中附带了+号

--- urldecode.cc
+++ urldecode.cc
@@ -3,23 +3,24 @@
 using namespace std;
 string UrlDecode(const string& szToDecode)
 {
-    string result;
-    int hex = 0;
-    for (size_t i = 0; i < szToDecode.length(); ++i)
+  string result;
+  int hex = 0;
+  for (size_t i = 0; i < szToDecode.length(); ++i)
     {
-        switch (szToDecode[i])
+      switch (szToDecode[i])
         {
-        case '+':
-            result += ' ';
-            break;
+          //        case '+':
+          //这里不需要+号编程空格
+          //result += ' ';
+          //          break;
         case '%':
-            if (isxdigit(szToDecode[i + 1]) && isxdigit(szToDecode[i + 2]))
+          if (isxdigit(szToDecode[i + 1]) && isxdigit(szToDecode[i + 2]))
             {
-                std::string hexStr = szToDecode.substr(i + 1, 2);
-                hex = strtol(hexStr.c_str(), 0, 16);
-                //字母和数字[0-9a-zA-Z]、一些特殊符号[$-_.+!*'(),] 、以及某些保留字[$&+,/:;=?@]
-                //可以不经过编码直接用于URL
-                if (!((hex >= 48 && hex <= 57) || //0-9
+              std::string hexStr = szToDecode.substr(i + 1, 2);
+              hex = strtol(hexStr.c_str(), 0, 16);
+              //字母和数字[0-9a-zA-Z]、一些特殊符号[$-_.+!*'(),] 、以及某些保留字[$&+,/:;=?@]
+              //可以不经过编码直接用于URL
+              if (!((hex >= 48 && hex <= 57) || //0-9
                     (hex >=97 && hex <= 122) ||   //a-z
                     (hex >=65 && hex <= 90) ||    //A-Z
                     //一些特殊符号及保留字[$-_.+!*'(),]  [$&+,/:;=?@]
@@ -28,23 +29,26 @@ string UrlDecode(const string& szToDecode)
                     || hex == 0x3A || hex == 0x3B|| hex == 0x3D || hex == 0x3f || hex == 0x40 || hex == 0x5f
                     ))
                 {
-                    result += char(hex);
-                    i += 2;
+                  if (hex == 0x2b)
+                  result += char(hex);
+                  i += 2;
                 }
-                else result += '%';
-            }else {
+              else {
                 result += '%';
-            }
-            break;
+              }
+
+            }else {
+            result += '%';
+          }
+          break;
         default:
-            result += szToDecode[i];
-            break;
+          result += szToDecode[i];
+          break;
         }
     }
-    return result;
+  return result;
 }
 
 int main( int argc, char *argv[]){
-
   std::cout << UrlDecode(argv[1]) << std::endl;
 }
